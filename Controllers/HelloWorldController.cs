0ab298645b579cfeba0c5c081e460981254387e3
LizCastanedaGarcia <w0781459@myscc.ca>
2023-10-09 21:47:04 -0400

    Created Model with attributes.

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,17 +8,27 @@ namespace MvcPlate.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
